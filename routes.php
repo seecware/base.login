e0b8b8a07ae89961966290b49f4cbeb4fbcb8f16
Francisco .M <seecware@gmail.com>
2025-05-07 13:17:13 -0600

    Adding new routes (Login and signup). Starting to manage logins with php sessions.

--- routes.php
+++ routes.php
@@ -5,6 +5,8 @@ return [
         '/contact' => ['StaticContentController', 'renderContent', 'contact', ["title" => "Contact"]],
         '/new-client' => ['StaticContentController', 'renderContent', 'new-client', ["title" => "Adding client..."]],
         '/new-added-client' => ['ClientsController', 'confirmation', 'new-added-client', ["title" => "Added!"]],
+        '/login' => ['StaticContentController', 'renderContent', 'login', ["title" => "Log in"]],
+        '/signup' => ['StaticContentController', 'renderContent', 'signup', ["title" => "Sign Up"]]
     ],
     'POST' => [
         '/new-client' => ['ClientsController', 'addNewClient'],
